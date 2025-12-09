788610997be4733af706132bdc3aa8822fc3d889
Student 071 pp25 <pp25s071@apollo-gpu.lsalab.cs.nthu.edu.tw>
2025-12-09 19:20:34 +0800

    block_mul_pal commit 1

--- block_mul_pal.cu
+++ block_mul_pal.cu
@@ -33,33 +33,97 @@ void correctness_check(){
     }
 }
 
+// grid = dim3(Bs,Bs), block = dim3(Bs,Bs)
+__global__ void block_mul_kernel(
+    int Bs,
+    int N,
+    int n,
+    float *d_A,
+    float *d_B,
+    float *d_C
+){
+    extern __shared__ float share[];
+    float *A_block = share;
+    float *B_block = share + Bs * Bs;
+
+    // block index 
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int A_start_row = bx * Bs;
+    int A_start_col;
+    int B_start_row;
+    int B_start_col = by * Bs;
+    
+    // block local index
+    int lx = threadIdx.y;
+    int ly = threadIdx.x;
+    int idx = lx * Bs + ly;
+
+    // intialized
+    float c = 0;
+
+
+    for(int bk = 0; bk< n ; bk++){
+        A_start_col = bk * Bs;
+        B_start_row = bk * Bs;
+        
+        // load A, B block
+        A_block[idx] = d_A[(A_start_row + lx) * N + A_start_col + ly];
+        B_block[idx] = d_A[(B_start_row + lx) * N + B_start_col + ly];
+
+        __syncthreads();
+
+        // compute c
+        for(int k = 0; k < Bs; k++){
+            c += A_block[lx * Bs + k] * B_block[ k * Bs + ly];
+        }
+
+        __syncthreads();
+
+    }
+    d_C[((bx * Bs) + lx) * N + (by * Bs) + ly ] = c;
+
+}
+
 int main(int argc, char* argv[]){
     assert(argc==2);
     N = atoi(argv[1]);
     create_matrix();
  
-    int T = 32; // tile size
-    int n = N / T;
-    assert(N % T ==0);
+    int Bs = 32; // Block size
+    int n = N / Bs;
+    assert(N % Bs ==0);
 
-     // c[ii, jj] = A[ii, kk] B[kk, jj] for all kk
-    for(int ii=0; ii<n; ii++){
-        for(int jj=0; jj<n; jj++){
-            for(int kk=0; kk<n ;kk++){
+    // cudaMalloc
+    float *d_A, *d_B, *d_C;
+    size_t gmem = N * N *  sizeof(float);
+    cudaMalloc((void **)&d_A, gmem);
+    cudaMalloc((void **)&d_B, gmem);
+    cudaMalloc((void **)&d_C, gmem);
+    
+    // cuda Memory copy
+    cudaMemcpy(d_A, A, gmem, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, gmem, cudaMemcpyHostToDevice);
 
-                // c[ii,jj][i,j] = A[ii, kk][i,k] B[kk, jj][k,j] for all kk
-                for(int i=0; i < T; i++ ){
-                    for(int j=0; j < T; j++){
-                        for(int k=0; k<T; k++){
-                            C[(ii*T + i)*N + (jj*T+j)] += (A[(ii*T+i)*N + (kk*T+k)] * B[(kk*T+k)*N + (jj*T+j)]);
-                        }
-                    }
-                    
-                }
-  
-            }
-        }
-    }
+    //launch kernel
+    size_t shmem = Bs * Bs * 2 * sizeof(float);
+    block_mul_kernel<<<dim3(n,n), dim3(Bs,Bs), shmem>>>(
+        Bs, 
+        N,
+        n,
+        d_A,
+        d_B,
+        d_C
+    );
+
+    //cuda Memory copy
+    cudaMemcpy(C, d_C, gmem, cudaMemcpyDeviceToHost);
+
+    //free cuda memeory
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
 
     correctness_check();
 
